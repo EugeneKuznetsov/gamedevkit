a258bcf6e2d00c2c1dadd3c04762c0e1e3b5c6a5
Eugene Kuznetsov <eugene.kuznetsov.de@gmail.com>
2022-07-13 10:17:37 +0200

    fix: update derived Game class in test package
    related to #15

--- test_package/main.cpp
+++ test_package/main.cpp
@@ -3,11 +3,15 @@
 #include <GDK/AbstractGame.hpp>
 #include <GDK/AbstractRenderer.hpp>
 #include <GDK/Application.hpp>
+#include <GDK/Keyboard.hpp>
 #include <GDK/WindowBuilder.hpp>
+
+namespace keyboard = gamedevkit::input::keyboard;
 
 class Game final : public gamedevkit::AbstractGame {
     auto setup() -> void override {}
     auto update() -> void override {}
+    auto input(const keyboard::Key&, const keyboard::Action&, const std::set<keyboard::Modifier>&) -> void override {}
 };
 
 class Renderer final : public gamedevkit::AbstractRenderer {
@@ -17,11 +21,10 @@ class Renderer final : public gamedevkit::AbstractRenderer {
 
 auto main() -> int
 {
-    gamedevkit::Application{}
-        .window(gamedevkit::WindowBuilder{"GameDevKitApp", {800, 600}}.opengl_profile_core().context_version(4, 0).build())
-        .game(std::make_shared<Game>())
-        .renderer(std::make_unique<Renderer>())
-        .setup();
-
+    try {
+        gamedevkit::Application{}.game(std::make_shared<Game>()).renderer(std::make_unique<Renderer>()).setup();
+    }
+    catch (...) {
+    }
     return EXIT_SUCCESS;
 }
