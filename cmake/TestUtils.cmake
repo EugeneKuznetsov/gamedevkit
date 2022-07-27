b69e3e7e0cfa5ada76432af18b925d0492a59bde
Eugene Kuznetsov <eugene.kuznetsov.de@gmail.com>
2022-07-27 23:34:41 +0200

    refactor: simplify GDK CMake target functions
    closes #33

--- cmake/TestUtils.cmake
+++ cmake/TestUtils.cmake
@@ -1,26 +1,38 @@
-function(add_test_suite)
+function(add_test_suite GDK_TEST_SUITE_NAME)
     set(options)
-    set(oneValueArgs NAME TARGET_UNDER_TEST)
-    set(multiValueArgs TEST_SUITE_DEPENDENCY_LIBS TARGET_UNDER_TEST_STUB_LIBS)
-    cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    set(oneValueArgs TARGET_UNDER_TEST)
+    set(multiValueArgs TEST_SUITE_DEPENDENCIES)
+    cmake_parse_arguments(GDK "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    set(MAIN_TARGET ${ARG_NAME})
-    set(SOURCES "${MAIN_TARGET}.cpp")
-    set(TARGET_UNDER_TEST ${ARG_TARGET_UNDER_TEST})
-    set(TARGET_UNDER_TEST_OBJECTS ${TARGET_UNDER_TEST}-object)
-    set(TARGET_UNDER_TEST_API ${TARGET_UNDER_TEST}-api)
-    set(TARGET_UNDER_TEST_DETAILS_API ${TARGET_UNDER_TEST}-details-api)
-    get_target_property(TARGET_UNDER_TEST_SOURCES ${TARGET_UNDER_TEST_OBJECTS} INTERFACE_SOURCES)
+    if(NOT TARGET ${GDK_TARGET_UNDER_TEST})
+        message(FATAL_ERROR "Target under test ${GDK_TARGET_UNDER_TEST} is not a target")
+    endif()
 
-    add_executable(${MAIN_TARGET} ${SOURCES} ${TARGET_UNDER_TEST_SOURCES})
-    target_link_libraries(
-        ${MAIN_TARGET}
-        PRIVATE
-            GTest::gtest_main GTest::gmock_main ${ARG_TARGET_UNDER_TEST_STUB_LIBS}
-            ${TARGET_UNDER_TEST_API}
-            $<$<TARGET_EXISTS:${TARGET_UNDER_TEST_DETAILS_API}>:${TARGET_UNDER_TEST_DETAILS_API}>)
-    foreach(DEPENDENCY_TARGET IN LISTS ARG_TEST_SUITE_DEPENDENCY_LIBS)
-        target_link_libraries(${MAIN_TARGET} PRIVATE ${DEPENDENCY_TARGET})
+    get_target_property(GDK_TARGET_UNDER_TEST_SOURCES ${GDK_TARGET_UNDER_TEST} INTERFACE_SOURCES)
+    if(NOT GDK_TARGET_UNDER_TEST_SOURCES)
+        message(
+            FATAL_ERROR
+                "Property INTERFACE_SOURCES of target under test ${GDK_TARGET_UNDER_TEST} is empty")
+    endif()
+
+    set(GDK_TEST_SUITE_SOURCES "${GDK_TEST_SUITE_NAME}.cpp")
+
+    add_executable(${GDK_TEST_SUITE_NAME} ${GDK_TEST_SUITE_SOURCES}
+                                          ${GDK_TARGET_UNDER_TEST_SOURCES})
+    target_link_libraries(${GDK_TEST_SUITE_NAME} PRIVATE GTest::gtest_main GTest::gmock glewstub)
+
+    get_target_property(TARGET_UNDER_TEST_INCLUDE_DIRECTORIES ${GDK_TARGET_UNDER_TEST}
+                        INTERFACE_INCLUDE_DIRECTORIES)
+    target_include_directories(${GDK_TEST_SUITE_NAME}
+                               PRIVATE ${TARGET_UNDER_TEST_INCLUDE_DIRECTORIES})
+
+    foreach(DEPENDENCY_TARGET IN LISTS GDK_TEST_SUITE_DEPENDENCIES)
+        string(REGEX MATCH "^(PRIVATE|PUBLIC|INTERFACE) (.*)" _ ${DEPENDENCY_TARGET})
+        if(NOT TARGET ${CMAKE_MATCH_2})
+            message(FATAL_ERROR "${CMAKE_MATCH_2} is not a target")
+        endif()
+        target_link_libraries(${GDK_TEST_SUITE_NAME} ${CMAKE_MATCH_1} ${CMAKE_MATCH_2})
     endforeach()
-    add_test(NAME ${MAIN_TARGET} COMMAND $<TARGET_FILE:${MAIN_TARGET}>)
+
+    add_test(NAME ${GDK_TEST_SUITE_NAME} COMMAND $<TARGET_FILE:${GDK_TEST_SUITE_NAME}>)
 endfunction()
